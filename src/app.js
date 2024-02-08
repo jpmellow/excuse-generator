085b0a5dc37c6bc2b3f1b31e5b49459f30d90c02
jp <152410055+jpmellow@users.noreply.github.com>
2024-02-08 01:59:31 +0000

    Done with project

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//create a random excuse
+
+const who = ["My dog ", "My grandma ", "His suegro ", "My bird "];
+const action = ["ate ", "peed on ", "broke ", "lost "];
+const what = ["my homework ", "the keys ", "the car ", "my sister "];
+const when = [
+  "before class ",
+  "right on time ",
+  "this morning ",
+  "during lunch "
+];
+
+function randomIndex(len) {
+  return Math.floor(Math.random() * len);
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("hi");
 };
+
+const indexWho = randomIndex(who.length);
+const indexAction = randomIndex(action.length);
+const indexWhat = randomIndex(what.length);
+const indexWhen = randomIndex(when.length);
+const excuse =
+  who[indexWho] + action[indexAction] + what[indexWhat] + when[indexWhen];
+
+document.querySelector("div").innerText = excuse;
